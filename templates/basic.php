f23d9e0311caae8bfb3ce38a6a6f6a4c9eb2e169
Rachel Fish <itsrachelfish@gmail.com>
2012-12-07 21:24:22 +0400

    Moving CSS/JS to separate files.

--- templates/basic.php
+++ templates/basic.php
@@ -6,44 +6,8 @@
 		<script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js'></script>
 		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/base/jquery-ui.css" type="text/css" />
 		
-		<style>
-			body {
-				background-color:#000;
-				font-family:Courier New;
-				font-size:10pt;
-				color:#eee;
-			}
-			
-			input, textarea {
-				display: block;
-				width:100%;
-				margin-bottom:10px;
-			}
-			
-			textarea {
-				min-height:200px;
-			}
-			
-			input[type='submit'] {
-				border-radius:4px;
-			}
-			
-			.error {
-				background-color:rgba(255, 0, 0, 0.3);
-				border:1px solid rgba(255, 0, 0, 0.5);
-				margin-bottom:20px;
-				padding:10px;
-			}
-			
-			.right { float: right; }
-			.left { float: left; }
-		</style>
-		
-		<script>
-			var RecaptchaOptions = {
-				theme : 'blackglass'
-			};
-		</script>
+		<link rel='stylesheet' href='/css/basic.css' type='text/css'>
+		<script src='/js/basic.js'></script>
 	</head>
 	
 	<body>
